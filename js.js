9a380293ec47d48083c14a46916f48f15a847f2d
DonavioNovelh <donavioev@gmail.com>
2024-11-02 00:24:43 +0200

    organizando o css

--- js.js
+++
@@ -1,3 +0,0 @@
-
-  
- console.log('ddsdsa'.replaceAll('d', ''))
--- jsq.js
+++
@@ -1,7 +0,0 @@
-for (let i = 0; i < display.textContent.length; i++) {
-    if(operador[display.textContent[i]]) {
-
-        display.textContent = display.textContent[i].toLowerCase
-    }
-    
-}
